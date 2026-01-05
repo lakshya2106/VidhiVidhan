9305395503c068388b64f83b53ec0aec8a97a0e3
Lakshyaraj Purbia <lakshyapurbia21@gmail.com>
2026-01-05 11:21:29 +0530

    feat:vite pwa1

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,34 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'Vidhi Vidhan',
-      short_name: 'VidhiVidhan',
-      description: 'A platform for legal document automation and management.',
-      theme_color: '#0f172a',
-      icons: [
-        {
-          src: '/VV-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/VV-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        }
-      ]
-    }
-  })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Vidhi Vidhan',
+        short_name: 'VidhiVidhan',
+        description: 'A platform for legal document automation and management.',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/VV-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/VV-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
